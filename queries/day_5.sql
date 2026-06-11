3247db3a336b2cf9833b11b79fa49c828318e89d
Sirisha-1792 <sirishamaragada@gmail.com>
2026-06-11 20:48:08 +0530

    DAY_5

---
+++ queries/day_5.sql
@@ -0,0 +1,86 @@
+use 50_days_sql_challenge;
+
+-- Invalid Age(min age: 18 & max age: 65)
+select * from cleaned_employees
+where age < 18 or age > 65;
+
+-- Invalid salary
+select * from cleaned_salaries
+where salary<0;
+
+-- Invalid ratings(from 1 to 5)
+select * from cleaned_performance
+where rating_46622 < 1 or rating_46622 > 5
+   or rating_46623 < 1 or rating_46623 > 5
+   or rating_46624 < 1 or rating_46624 > 5;
+
+-- Fix invalid age
+update cleaned_employees
+set age = 
+case 
+when emp_id = 20 then 32
+when emp_id = 24 then 28
+when emp_id = 44 then 35
+when emp_id = 65 then 40
+when emp_id = 66 then 30
+when emp_id = 67 then 27
+when emp_id = 110 then 38
+when emp_id = 134 then 29
+when emp_id = 147 then 33
+when emp_id = 188 then 36
+when emp_id = 216 then 31
+when emp_id = 223 then 26
+when emp_id = 266 then 42
+when emp_id = 295 then 34
+when emp_id = 318 then 28
+when emp_id = 347 then 37
+when emp_id = 360 then 39
+when emp_id = 387 then 25
+when emp_id = 420 then 41
+when emp_id = 428 then 33
+when emp_id = 440 then 29
+when emp_id = 460 then 36
+when emp_id = 466 then 27
+when emp_id = 469 then 32
+when emp_id = 471 then 35
+
+else age
+END
+where age < 18 or age > 65;
+
+update cleaned_salaries
+set salary = 
+case 
+when emp_id = 463 then 55000
+when emp_id = 142 then 60000
+when emp_id = 100 then 45000
+when emp_id = 30 then 70000
+when emp_id = 54 then 52000
+when emp_id = 310 then 48000
+when emp_id = 183 then 51000
+when emp_id = 168 then 47000
+when emp_id = 126 then 46000
+when emp_id = 206 then 58000
+when emp_id = 76 then 49000
+when emp_id = 433 then 62000
+when emp_id = 43 then 53000
+when emp_id = 324 then 55000
+when emp_id = 226 then 51000
+when emp_id = 113 then 50000
+when emp_id = 273 then 52000
+when emp_id = 200 then 48000
+when emp_id = 160 then 47000
+when emp_id = 42 then 46000
+when emp_id = 150 then 49000
+when emp_id = 45 then 53000
+when emp_id = 65 then 54000
+when emp_id = 488 then 60000
+when emp_id = 8 then 45000
+when emp_id = 121 then 50000
+when emp_id = 78 then 47000
+when emp_id = 94 then 46000
+when emp_id = 353 then 52000
+
+else salary
+END
+where salary<0;
