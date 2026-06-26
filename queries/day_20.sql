cf3cf8d0f6d2b83e61bc10dd68ea18dbcfc3b1af
Sirisha-1792 <sirishamaragada@gmail.com>
2026-06-26 22:30:19 +0530

    DAY_20

---
+++ queries/day_20.sql
@@ -0,0 +1,37 @@
+use 50_days_sql_challenge;
+
+--1. latest salary
+select * from
+(select *, row_number() 
+    over (partition by emp_id
+    order by salary_date desc) 
+    as rn 
+from cleaned_salaries) new_table
+where rn = 1;
+
+--2. employees first salary
+select * from
+(select *, row_number() 
+    over (partition by emp_id
+    order by salary_date asc) 
+    as rn 
+from cleaned_salaries) new_table
+where rn = 1;
+
+--3. Rank salary entries (rank salaries for each employee)
+select * from
+(select *, row_number()
+    over (PARTITION BY emp_id
+    order by salary_date asc)
+    as rank_number
+from cleaned_salaries) new_table
+where rank_number = 1;
+
+--4. get Top 2 salary records per employee
+select * from
+(select *, row_number()
+    over (PARTITION BY emp_id
+    order by salary_date asc)
+    as rank_number
+from cleaned_salaries) new_table
+where rank_number <= 2;
